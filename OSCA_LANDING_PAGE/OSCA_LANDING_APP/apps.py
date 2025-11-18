cc857115a1faac01bc042c205c35aed03790aa48
BRYNX2005 <lonielgapol20@gmail.com>
2025-11-18 22:37:30 +0800

    2nd commit

---
+++ OSCA_LANDING_PAGE/OSCA_LANDING_APP/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class OscaLandingAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'OSCA_LANDING_APP'
--- OSCA_WEB_PAGE/OSCA_WEB_APP/views.py
+++ OSCA_WEB_PAGE/OSCA_WEB_APP/views.py
@@ -2,5 +2,8 @@ from django.shortcuts import render
 
 
 def Dashboard(request):
-    return render(request, 'OSCA_WEB_APP/dashboard.html')
+    return render(request, 'dashboard_admin.html')
+
+def Registration_Category(request):
+    return render(request, 'Registration_Category.html')
 
